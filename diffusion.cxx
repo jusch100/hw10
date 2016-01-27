cbe89950356b4a160977d34f394dd808e6bd03f9
Julian Schmitt <julian@linux-k0d8.site>
2016-01-27 02:55:28 +0100

    done

--- diffusion.cxx
+++ diffusion.cxx
@@ -24,7 +24,7 @@ int main(){
   const double xmax = 20;
   const double dx = (xmax-xmin)/(N-1) ;
 
-  double dt = dx;
+  double dt = dx*dx/(1.8*D);// nenner kleiner 2D: unstabil, größer oder gleich 2D: stabil
   double t = 0;
   const int Na = 10;
   const int Nk = int(tEnd/Na/dt);
@@ -44,8 +44,13 @@ int main(){
   for(int i=1; i<=Na; i++)
   {
    for(int j=0; j<Nk; j++){
-
-
+     
+    step(u1,u0,dt,dx,D,N);
+    h=u0;
+    u0=u1;
+    u1=h;
+    t+=dt;
+  
    }
    strm.str("");
    strm << "u_" << i;
@@ -63,7 +68,11 @@ void step(double* const f1, double* const f0,
           const double dt, const double dx,
           const double D, const int N)
 {
-
+    f1[0] = f0[0] + D*dt/(dx*dx) * (f0[1]-2*f0[0]+f0[N-1]);
+    for(int i = 1; i<N-1; i++){
+      f1[i] = f0[i] + D*dt/(dx*dx) * (f0[i+1]-2*f0[i]+f0[i-1]);
+    }
+    f1[N-1] = f0[N-1] + D*dt/(dx*dx) * (f0[0]-2*f0[N-1]+f0[N-2]);
 }
 //-----------------------------------------------
 void initialize(double* const u0, const double dx,
